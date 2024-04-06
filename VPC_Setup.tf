e8704f573df5c3eb5173f1f38e8f92cd4605805d
Suraj Pandey <suraj945688@gmail.com>
2024-04-07 00:13:33 +0530

    Terraform VPC and EC2 code

---
+++ VPC_Setup.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+  region     = "ap-south-1"
+  access_key = "AKIAT6IWH4I74ZKAFXVI"
+  secret_key = "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"
+}
+resource "aws_s3_bucket" "firstbucket" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
