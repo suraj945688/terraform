29f0bb33f6ee734cc0e8c53e60ecde3a436fbaa4
Suraj Pandey <suraj945688@gmail.com>
2024-04-07 00:21:59 +0530

    Terraform vpc and subnet code

--- VPC_Setup.tf
+++ VPC_Setup.tf
@@ -1,7 +1,7 @@
 provider "aws"{
   region     = "ap-south-1"
-  access_key = "AKIAT6IWH4I74ZKAFXVI"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 resource "aws_s3_bucket" "firstbucket" {
   bucket = "my-tf-test-bucket"
--- instance.tf
+++ instance.tf
@@ -1,8 +1,3 @@
-provider "aws"{
-  region     = "ap-south-1"
-  access_key = "AKIAT6IWH4I74ZKAFXVI"
-  secret_key = "4QYLtbtt9tQz1zIQoy0o9ClY8Y/P9gDL39EapnDZ"
-}
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
 
