17426138fbe53d0e2bd11bb28ed90dcb7fe9ea9a
Suraj Pandey <suraj945688@gmail.com>
2024-04-09 23:40:59 +0530

    vpc with nat gw

--- VPC_Setup.tf
+++ VPC_Setup.tf
@@ -1,8 +1,59 @@
-resource "aws_s3_bucket" "firstbucket" {
-  bucket = "my-tf-test-bucket"
+resource "aws_instance" "MyIMG" {
+  ami           = "ami-09298640a92b2d12c"
+  instance_type = "t2.micro"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "Testing Machine"
+  }
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Terraform_VPC"
+  }
+}
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Private_Subnet"
+  }
+}
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public_Subnet"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.private.id
+	tags= {
+	  Name = "Private Connection"
   }
 }
